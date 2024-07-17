54ecace0fbbc0430783079d3f08ea88bd5ca9ba9
OlgaKosukhina <heeeelga@gmail.com>
2024-07-17 19:47:18 +0300

    Исправления после ревью

--- js/main.js
+++ js/main.js
@@ -1,8 +1,25 @@
-const PHOTO_ARRAY_LENGTH = 25;
-const MIN_LIKES = 15;
-const MAX_LIKES = 200;
-const MIN_COMMENTS = 0;
-const MAX_COMMENTS = 30;
+const PHOTO_COUNT = 25;
+
+const Like = {
+  MIN: 15,
+  MAX: 200
+};
+
+const Comment = {
+  MIN: 0,
+  MAX: 30
+};
+
+const CommentIdRange = {
+  MIN: 1,
+  MAX: 57
+};
+
+const Avatar = {
+  MIN: 1,
+  MAX: 6
+};
+
 const DESCRIPTIONS = ['Bonjour!', 'Это я в отпуске.', 'Это мой кот. Он красивый.', 'Привет. Как дела?', 'Я и закат.', 'Очень усердно пишу код.', 'На даче с бабушкой. Набрал уже 10кг.'];
 const COMMENTS = ['Всё отлично!', 'В целом всё неплохо. Но не всё.', 'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.', 'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.', 'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.', 'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'];
 const NAMES = [
@@ -47,31 +64,31 @@ const createRandomId = (min, max) => {
 const getRandomArrayElement = (elements) => elements[getRandomInteger(0, elements.length - 1)];
 
 const createComment = () => {
-  const commentId = createRandomId(1, 57);
+  const commentId = createRandomId(CommentIdRange.MIN, CommentIdRange.MAX);
 
-  return () => {
-    const comments = {};
-    comments.id = commentId();
-    comments.avatar = `img/avatar-${getRandomInteger(1, 6)}.svg`;
-    comments.message = getRandomArrayElement(COMMENTS);
-    comments.name = getRandomArrayElement(NAMES);
-    return comments;
-  };
+  return () => ({
+    id: commentId(),
+    avatar: `img/avatar-${getRandomInteger(Avatar.MIN, Avatar.MAX)}.svg`,
+    message: getRandomArrayElement(COMMENTS),
+    name: getRandomArrayElement(NAMES),
+  });
 };
 
 const createPhotos = () => {
-  const photoId = createRandomId(1, 25);
-  const urlId = createRandomId(1, 25);
+  const getId = createRandomId(1, PHOTO_COUNT);
 
   return () => {
-    const photo = {};
-    photo.id = photoId();
-    photo.url = `photos/${urlId()}.jpg`;
-    photo.description = getRandomArrayElement(DESCRIPTIONS);
-    photo.likes = getRandomInteger(MIN_LIKES, MAX_LIKES);
-    photo.comments = Array.from({length: getRandomInteger(MIN_COMMENTS, MAX_COMMENTS)}, createComment());
-    return photo;
+    const id = getId();
+
+    return {
+      id,
+      url: `photos/${id}.jpg`,
+      description: getRandomArrayElement(DESCRIPTIONS),
+      likes: getRandomInteger(Like.MIN, Like.MAX),
+      comments: Array.from({length: getRandomInteger(Comment.MIN, Comment.MAX)}, createComment()),
+    };
   };
 };
 
-Array.from({length: PHOTO_ARRAY_LENGTH}, createPhotos());
+const getPhotos = () => Array.from({length: PHOTO_COUNT}, createPhotos());
+getPhotos();
