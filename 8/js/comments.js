1210f8799f05bb45df012b8a22361567e516480d
Keks <keks@htmlacademy.ru>
2024-09-16 20:21:04 +0300

    :heavy_check_mark: Сборка #8

--- 8/js/comments.js
+++ 8/js/comments.js
@@ -2,21 +2,18 @@ const COMMENTS_COUNT = 5;
 let currentCount = 0;
 let comments = [];
 
-const bigPictureElement = document.querySelector('.big-picture');
-const socialCommentsElement = bigPictureElement.querySelector('.social__comments');
+const socialCommentsElement = document.querySelector('.social__comments');
 const commentTemplateElement = socialCommentsElement.querySelector('.social__comment');
-const commentsTotalElement = bigPictureElement.querySelector('.social__comment-count');
+const commentsTotalElement = document.querySelector('.social__comment-count');
 const commentsCountElement = commentsTotalElement.querySelector('.social__comment-shown-count');
 const commentsTotalCountElement = commentsTotalElement.querySelector('.social__comment-total-count');
-const commentsLoaderElement = bigPictureElement.querySelector('.comments-loader');
+const commentsLoaderElement = document.querySelector('.comments-loader');
 socialCommentsElement.innerHTML = '';
 
-const renderNextComments = () => {
+const getCommentElement = (commentElements) => {
   const fragment = document.createDocumentFragment();
-  const renderedComments = comments.slice(currentCount, currentCount + COMMENTS_COUNT);
-  const renderedCommentsLength = renderedComments.length + currentCount;
 
-  renderedComments.forEach(({avatar, name, message}) => {
+  commentElements.forEach(({avatar, name, message}) => {
     const commentElement = commentTemplateElement.cloneNode(true);
 
     commentElement.querySelector('.social__picture').src = avatar;
@@ -27,29 +24,40 @@ const renderNextComments = () => {
   });
 
   socialCommentsElement.appendChild(fragment);
-  commentsCountElement.textContent = renderedCommentsLength;
-  commentsTotalCountElement.textContent = comments.length;
+};
 
-  if (renderedCommentsLength >= comments.length) {
+const renderNextComments = () => {
+  const nextComments = comments.slice(currentCount, currentCount + COMMENTS_COUNT);
+  const newCommentsCount = nextComments.length + currentCount;
+
+  getCommentElement(nextComments);
+
+  commentsCountElement.textContent = newCommentsCount;
+
+  if (newCommentsCount >= comments.length) {
     commentsLoaderElement.classList.add('hidden');
   }
 
-  currentCount += COMMENTS_COUNT;
-
+  currentCount += nextComments.length;
 };
 
-const clearComments = () => {
+const resetComments = () => {
   currentCount = 0;
   socialCommentsElement.innerHTML = '';
   commentsLoaderElement.classList.remove('hidden');
-  commentsLoaderElement.removeEventListener('click', renderNextComments);
+  commentsLoaderElement.removeEventListener('click', onCommentsLoaderClick);
 };
 
 const renderComments = (pictureComments) => {
   comments = pictureComments;
   renderNextComments();
 
-  commentsLoaderElement.addEventListener('click', renderNextComments);
+  commentsTotalCountElement.textContent = comments.length;
+  commentsLoaderElement.addEventListener('click', onCommentsLoaderClick);
 };
 
-export { renderComments, clearComments };
+function onCommentsLoaderClick() {
+  renderNextComments();
+}
+
+export { renderComments, resetComments };
